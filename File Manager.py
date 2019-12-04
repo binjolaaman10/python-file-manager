3686dbae073888587cf46076c024fa4df0c4bb43
aman binjola <binjolaaman10@gmail.com>
2019-12-05 00:12:54 +0530

    added back functionality

--- File Manager.py
+++ File Manager.py
@@ -5,67 +5,102 @@ as a personal project.
 28-November-2019
 '''
 
+# ! python3
+
+import sys
 import os
+import shutil
+# import send2trash
 
 print('Welcome to Zolo File Manager\n')
-
-# Function to check if folders are left in the current directory.
-def noFoldersLeft():
-    for x in os.getcwd():
-        if os.path.isdir():
-            return False
-    return True
 
 # Stores every drive connected on PC in a list.
 drives = [chr(x) + ':' for x in range(65, 90) if os.path.exists(chr(x) + ':')]
 
-# Home Screen
-print('\nQuick Acess:\n1. Documents\n2. Videos\n3. Pictures\n4. Downloads\n')
-
-print('Drives: ')
-for x in range(len(drives)):
-    print(str(5 + x) + '. ' + drives[x])
-
 while True:
-    inp = input("\nEnter your Choice: ")
+    print("1.Open files/folders \n2.Rename \n3.Move and Paste \n4.Copy and Paste \n5.Delete\n")
+    result = input("Choose one of the following: ")
 
-    if inp == '1':
-        path = 'C:\\Users\\$USERNAME\\Documents'
-        os.chdir(os.path.expandvars(path))
-        break;
-    elif inp == '2':
-        path = 'C:\\Users\\$USERNAME\\Videos'
-        os.chdir(os.path.expandvars(path))
-        break;
-    elif inp == '3':
-        path = 'C:\\Users\\$USERNAME\\Pictures'
-        os.chdir(os.path.expandvars(path))
-        break;
-    elif inp == '4':
-        path = 'C:\\Users\\$USERNAME\\Downloads'
-        os.chdir(os.path.expandvars(path))
-        break;
-    elif inp in drives:
-        os.chdir(inp + '\\')
-        break;
-    else:
-        print('Error\nEnter a correct input.\n')
+    if result == '1':
+        # Home Screen
+        print('\nQuick Acess:\n1. Documents\n2. Videos\n3. Pictures\n4. Downloads\n')
 
+        print('Drives: ')
+        for x in range(len(drives)):
+            print(str(5 + x) + '. ' + drives[x])
 
-print(os.getcwd(), '\n\n')
+        while True:
+            inp = input("\nEnter your Choice: ")
 
-listdir = os.listdir(os.getcwd())
-for x in os.listdir(os.getcwd()):
-    print(x)
+            if inp == '1':
+                path = 'C:\\Users\\$USERNAME\\Documents'
+                os.chdir(os.path.expandvars(path))
+                break
 
-res = input('\n\nChoose a file/folder: ')
-print('\n')
-if res in os.listdir(os.getcwd()):
-    if os.path.isfile(res):
-        os.system(res)
-    else:
-        os.chdir(res)
-        for x in os.listdir(os.getcwd()):
-            print(x)
-else:
-    print('No file exist of this name.')
+            elif inp == '2':
+                path = 'C:\\Users\\$USERNAME\\Videos'
+                os.chdir(os.path.expandvars(path))
+                break
+
+            elif inp == '3':
+                path = 'C:\\Users\\$USERNAME\\Pictures'
+                os.chdir(os.path.expandvars(path))
+                break
+
+            elif inp == '4':
+                path = 'C:\\Users\\$USERNAME\\Downloads'
+                os.chdir(os.path.expandvars(path))
+                break
+
+            elif inp in drives:
+                os.chdir(inp + '\\')
+                break
+
+            else:
+                print('Error\nEnter a correct input / drive name.\n')
+
+        while True:
+
+            listdir = os.listdir(os.getcwd())
+            for x in os.listdir(os.getcwd()):
+                print(x)
+
+            print('\n\nType "exitManager" to exit from file manager.')
+            print('Type "backManager" to go up one directory.')
+            res = input('\nChoose a file/folder: ')
+            print('\n')
+
+            if res in os.listdir(os.getcwd()):
+                if os.path.isfile(res):
+                    os.system(res)
+                else:
+                    os.chdir(res)
+                    for x in os.listdir(os.getcwd()):
+                        print(x)
+
+            elif res == 'exitManager':                                             # Exit command to exit from loop
+                sys.exit(1)
+
+            elif res == 'backManager':                                             # Back command to go up one directory
+                os.chdir('..')
+
+            else:
+                print('No file exist of this name.')
+
+    if result == '5':
+        while True:
+
+            # Options to delete files/folders to permanently or otherwise
+            print('\n\n1. Permanently \n2. Recycle Bin')
+            query = input('Would you like to permanently delete or send to Recycle Bin?: ')
+
+            if query == '1':
+                print('You chose to permanently delete files/folders.')
+                # code
+
+            elif query == '2':
+                print('You chose to temporarily delete files/folders.')
+                # code
+
+            else:
+                print('You chose wrong number')
